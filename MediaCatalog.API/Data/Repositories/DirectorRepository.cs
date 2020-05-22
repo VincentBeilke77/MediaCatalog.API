4ca1a88870b1e9f8a30a4927e6ed5a7f4f7fd905
Vincent Beilke <vincent.beilke@gmail.com>
2020-05-22 13:02:12 -0500

    Created Director interface and repository for retrieving director data.

---
+++ MediaCatalog.API/Data/Repositories/DirectorRepository.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MediaCatalog.API.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace MediaCatalog.API.Data.Repositories
+{
+    public class DirectorRepository : BaseRepository, IDirectorRepository
+    {
+        private readonly MediaCatalogContext _context;
+        private readonly ILogger<BaseRepository> _logger;
+
+        public DirectorRepository(MediaCatalogContext context, ILogger<BaseRepository> logger) : base(context, logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Director[]> GetAllDirectorsAsync()
+        {
+            _logger.LogInformation($"Getting all directors.");
+
+            IQueryable<Director> query = _context.Directors
+                .Include(dm => dm.DirectorMovies)
+                .ThenInclude(m => m.Movie);
+
+            query = query.OrderBy(d => d.FullName);
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Director> GetDirectorAsync(int directorId)
+        {
+            _logger.LogInformation($"Getting director info for {directorId}");
+
+            IQueryable<Director> query = _context.Directors
+                .Include(dm => dm.DirectorMovies)
+                .ThenInclude(m => m.Movie);
+
+            query = query.Where(d => d.Id == directorId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Director[]> GetDirectorsByMovieIdAsync(int movieId)
+        {
+            _logger.LogInformation($"Getting director info for {movieId}");
+
+            IQueryable<Director> query = _context.Directors
+                .Include(dm => dm.DirectorMovies)
+                .ThenInclude(m => m.Movie);
+
+            query = query.Where(dm => dm.DirectorMovies.Any(m => m.MovieId == movieId));
+
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Director> GetDirectorByNameAsync(string lastName, string firstName)
+        {
+            _logger.LogInformation($"Getting director info for {lastName},{firstName}");
+
+            IQueryable<Director> query = _context.Directors
+                .Include(dm => dm.DirectorMovies)
+                .ThenInclude(m => m.Movie);
+
+            query = query.Where(d => d.LastName == lastName && d.FirstName == firstName);
+
+            return await query.FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ MediaCatalog.API/Data/Repositories/IDirectorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using MediaCatalog.API.Data.Entities;
+
+namespace MediaCatalog.API.Data.Repositories
+{
+    public interface IDirectorRepository
+    {
+        Task<Director[]> GetAllDirectorsAsync();
+
+        Task<Director> GetDirectorAsync(int directorId);
+
+        Task<Director[]> GetDirectorsByMovieIdAsync(int movieId);
+
+        Task<Director> GetDirectorByNameAsync(string lastName, string firstName);
+    }
+}
